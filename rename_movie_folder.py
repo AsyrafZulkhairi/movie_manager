ab668a15e44f84874cfb9c105442cd758284b861
Asyraf Zulkhairi <asyrafzulkhairi037@gmail.com>
2020-03-21 14:34:50 +0800

    remove words after codec like H264; prevent multiple bracket [];

--- rename_movie_folder.py
+++ rename_movie_folder.py
@@ -1,22 +1,25 @@
-# folder name: Inception.2010.1080p.x264
+# folder name: Inception.2010.1080p.x264.AAC-RARBG
 import os
 import sys
 import re
 
-print("Before:")
+print("Before:-")
 print(sys.argv[1])
 string = sys.argv[1]
 string = string.replace("."," ")
 #print(string)
-string = string.replace("1080p","[1080p]")
-string = string.replace("720p","[720p]")
+string = string.replace(" 1080p"," [1080p]")
+string = string.replace(" 720p"," [720p]")
 string = string.replace(" BluRay","")
-string = string.replace(" H264","")
+string = string.replace(" Bluray","")
 #print(string)
 
-string = re.sub('20(..)',r'(20\1)',string)
-string = re.sub('19(..)',r'(19\1)',string)
-print('After:')
+string = re.sub(' 20(..)',r' (20\1)',string)
+string = re.sub(' 19(..)',r' (19\1)',string)
+string = re.sub(' x264.+','',string)
+string = re.sub(' H264.+','',string)
+string = re.sub(' x265.+','',string)
+print('After:-')
 print(string)
 
 os.rename(sys.argv[1],string)
