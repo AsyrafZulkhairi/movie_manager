e8e83bf5b1494062eb36fc8e024161c899201578
Asyraf Zulkhairi <asyrafzulkhairi037@gmail.com>
2025-08-30 19:45:18 +0800

    Find resolution & year before rebuilding folder name.

--- rename_movie_folder.py
+++ rename_movie_folder.py
@@ -1,26 +1,48 @@
-# folder name: Inception.2010.1080p.x264.AAC-RARBG
+# Usage: rename_movie_folder.py <folder>
+# Date modified: 30 Aug 2025
 import os
 import sys
 import re
 
-print("Before:-")
-print(sys.argv[1])
 string = sys.argv[1]
-string = string.replace("."," ")
-#print(string)
-string = string.replace(" 1080p"," [1080p]")
-string = string.replace(" 720p"," [720p]")
-string = string.replace(" BluRay","")
-string = string.replace(" Bluray","")
-#print(string)
+print("Before:-")
+print(string)
 
-string = re.sub(' 20(..)',r' (20\1)',string)
-string = re.sub(' 19(..)',r' (19\1)',string)
-string = re.sub(' x264.+','',string)
-string = re.sub(' H264.+','',string)
-string = re.sub(' x265.+','',string)
+
+# Find resolution
+resolution = 0
+for pixel in ["2160p", "1080p", "720p"]:
+   if string.find(pixel) != -1:
+      resolution = pixel
+      string = string[:string.find(pixel)]
+
+
+
+# Find year
+year = re.findall(r'\b(1[0-9]{3}|2[0-9]{3})\b', string)
+year = year[-1]
+
+# Find title
+match = re.search(re.escape(year), string)
+string = string[:match.start()]
+index = -1
+found_index = 1
+for char in reversed(string):
+   if char.isalnum():
+      found_index = index
+      break
+   else:
+      index -= 1
+string = string[:found_index + 1]
+string = string.replace("."," ")
+
+#Append year & resolution
+print(f"year:{year} resolution:{resolution}")
+string += f" ({year}) [{resolution}]"
+
 print('After:-')
 print(string)
 
 os.rename(sys.argv[1],string)
 print("Done.")
+
